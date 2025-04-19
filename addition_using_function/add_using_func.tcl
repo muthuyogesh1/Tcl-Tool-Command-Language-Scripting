6752f291fd3fdcf89d3f442826fc7f794b5798e7
Muthuramalingam B <145793256+muthuyogesh1@users.noreply.github.com>
2025-04-19 22:10:51 +0530

    Create add_using_func.tcl

---
+++ addition_using_function/add_using_func.tcl
@@ -0,0 +1,14 @@
+proc sum of 2nos (n1 n2} {
+  set sum [expr $n1 + $n2]
+  puts "Sum of the $n1 and $n2 is $sum"
+}
+sum of 2nos 2 34
+sum of 2nos 78 67
+
+
+proc sum of 3nos (n1 n2 n3} {
+  set sum [expr $n1 + $n2 + $n3] 
+  puts "Sum of the $n1 and $n2 and $n3 is $sum"
+}
+sum of 3nos 2 34 234
+sum of 3nos 75 57 32
