d1edc4a30364fa486c414e1e945db0501c632459
Muthuramalingam B <145793256+muthuyogesh1@users.noreply.github.com>
2025-04-19 22:16:09 +0530

    Update using_stdin_for_inputs.tcl

--- using_stdin_for_inputs/using_stdin_for_inputs.tcl
+++ using_stdin_for_inputs/using_stdin_for_inputs.tcl
@@ -1,9 +1,10 @@
 proc sum of 2nos {n1 n2) {
-  set sum [expr $n1 + $n2] puts "Sum of the $n1 and $n2 is $sum"
+  set sum [expr $n1 + $n2] 
+  puts "Sum of the $n1 and $n2 is $sum"
 }
 
 puts "Enter the 2 inputs of seq"
-set a [gets stdin);
-set b [gets stdin);
+set a [gets stdin];
+set b [gets stdin];
 
 sum of 2nos $a $b
